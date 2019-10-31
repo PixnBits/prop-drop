443b8a27ed8e6d5012cdbba74c977404321de188
N Oliver <github@pixnbits.org>
2019-10-31 16:48:35 -0700

    fix(index): use new speed, fix random window

--- index.js
+++ index.js
@@ -17,19 +17,20 @@ board.on("ready", () => {
   var speed = 0;
 
   function random(min, max) {
-    return Math.random() * min + (max - min);
+    return Math.random() * (max - min) + min;
   }
 
   function newDirection() {
     var newSpeed = random(0.005, 0.15);
-    // running at 0.15 for x gives us 50cm of length, so x is about 340
-    var timeToRun = Math.round(newSpeed * 340);
+    // running at 0.15 for 5s gives us 50cm of length, 0.15 * 5 * x = 50cm, so x is about 67
+    var timeToRun = Math.round(newSpeed * 67);
     if (speed > 0) {
       newSpeed *= -1;
     }
+    speed = newSpeed;
 
     console.log(`running at ${speed} for ${timeToRun}ms`);
-    setTimeout(newDirection, timeToRun);
+    setTimeout(newDirection, timeToRun * 1e3);
     if (speed >= 0) {
       motor.cw(speed);
     } else {
