076bced2d7411d6106e67a37636515d6297a59aa
N Oliver <github@pixnbits.org>
2019-10-31 17:08:49 -0700

    feat(index): pause for a time when changing direction

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const {Board, Servo} = require("johnny-five");
 const Raspi = require("raspi-io").RaspiIO;
 const board = new Board({ io: new Raspi() });
 
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 console.log('starting');
 board.on("ready", () => {
   console.log("Connected");
@@ -15,29 +19,38 @@ board.on("ready", () => {
   motor.stop();
   board.on("exit", () => { motor.stop(); });
 
-  var speed = 0;
-
-  function random(min, max) {
-    return Math.random() * (max - min) + min;
-  }
+  var speedHistory = [0, 0];
 
   function newDirection() {
-    const newSpeed = (speed > 0) ? (
-      // reverse
-      -1 * random(0.005, 0.15)
-    ) : (
-      // forward
-      random(0.005, 0.15)
-    );
-    // running at 0.15 for 5s gives us 50cm of length, 0.15 * 5 * x = 50cm, so x is about 67
-    // but that's unmeasured, so adjusting from that guess
-    const timeToRun = Math.round(Math.abs(newSpeed) * 25);
-    speed = newSpeed;
+    console.log('speedHistory', speedHistory);
+    var timeToRun;
+    if (speedHistory[0] !== 0) {
+      // pause at the current location
+      speedHistory.unshift(0);
+      timeToRun = Math.round(random(1, 10));
+      console.log(`stopping for ${timeToRun}s`);
+      motor.stop();
+    } else {
+      const newSpeed = (speed > 0) ? (
+        // reverse
+        -1 * random(0.005, 0.15)
+      ) : (
+        // forward
+        random(0.005, 0.15)
+      );
+      // running at 0.15 for 5s gives us 50cm of length, 0.15 * 5 * x = 50cm, so x is about 67
+      // but that's unmeasured, so adjusting from that guess
+      timeToRun = Math.round(Math.abs(newSpeed) * 25);
+      speedHistory.unshift(newSpeed);
 
-    console.log(`running at ${speed} for ${timeToRun}s`);
+      console.log(`running at ${speed} for ${timeToRun}s`);
+      // johnny-five handles the direction for us
+      motor.cw(speed);
+    }
     setTimeout(newDirection, timeToRun * 1e3);
-    // johnny-five handles the direction for us
-    motor.cw(speed);
+    while (speedHistory.length > 2) {
+      speedHistory.pop();
+    }
   }
 
   setTimeout(newDirection, 3e3);
